79f602c1b2c4cbd286e85ff2253acadfecd9a8c0
TchouTchouThom <124408554+ThcouTchouThom@users.noreply.github.com>
2024-12-06 18:37:26 +0100

    Update HomeFragment.kt
    XY Plot added

--- application/app/src/main/java/fr/kodelab/banking/ui/home/HomeFragment.kt
+++ application/app/src/main/java/fr/kodelab/banking/ui/home/HomeFragment.kt
@@ -10,12 +10,18 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import fr.kodelab.banking.databinding.FragmentHomeBinding
 import fr.kodelab.banking.db.UserDAO
+import android.graphics.*;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYSeries;
+import com.androidplot.xy.*;
+import fr.kodelab.banking.R
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
     private lateinit var userDAO: UserDAO
+    private lateinit var plot: XYPlot
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,6 +47,21 @@ class HomeFragment : Fragment() {
         }
 
         // Add any additional setup for the placeholder square if needed
+        plot = view.findViewById(R.id.viewGraphPlaceholder);
+
+        val domainLabels = arrayOf(1, 2, 3, 6, 7, 8, 9, 10, 13, 14)
+        val series1Numbers = arrayOf(1, 4, 2, 8, 88, 16, 8, 32, 16, 64)
+
+        val series1: XYSeries = SimpleXYSeries(
+            series1Numbers.asList(),
+            SimpleXYSeries.ArrayFormat.Y_VALS_ONLY,
+            "Series 1"
+        )
+
+        val series1Format = LineAndPointFormatter(Color.RED, Color.GREEN, null, null)
+        plot.addSeries(series1, series1Format)
+
+        plot.legend.isVisible = false
     }
 
     override fun onDestroyView() {
