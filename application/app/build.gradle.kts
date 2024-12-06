e3a3fa821785753bdbe560fa2222d148e2fedd0f
TchouTchouThom <124408554+ThcouTchouThom@users.noreply.github.com>
2024-12-06 18:30:57 +0100

    Update build.gradle.kts
    added :
    implementation ("com.androidplot:androidplot-core:1.5.11")
    
    For graph in dashboard

--- application/app/build.gradle.kts
+++ application/app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.cardview)
+    implementation ("com.androidplot:androidplot-core:1.5.11")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
